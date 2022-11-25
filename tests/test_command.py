20281d30d6831c8d50f942f03aba0e737bc898a2
Sergey Vartanov <me@enzet.ru>
2022-11-25 11:51:26 +0400

    Add test for listening music

--- tests/test_command.py
+++ tests/test_command.py
@@ -3,6 +3,7 @@ from datetime import timedelta
 from chronicle.event.core import Event
 from chronicle.event.listen import (
     Interval,
+    ListenMusicEvent,
     ListenPodcastEvent,
 )
 from chronicle.event.timeline import Timeline
@@ -24,3 +25,20 @@ def test_listen_podcast() -> None:
     assert event.interval == Interval(
         from_=timedelta(minutes=10), to_=timedelta(minutes=20)
     )
+
+
+def test_listen_music() -> None:
+    """Test listening song command with title, artist, and album."""
+
+    timeline: Timeline = Timeline()
+    timeline.parse_command(
+        "2022-01-01T13:00:00/2022-01-01T14:00:00 song Strawberry Fields Forever by The Beatles "
+        "on Magic Mystery Tour"
+    )
+
+    assert len(timeline) == 1
+    event: Event = timeline.events[0]
+    assert isinstance(event, ListenMusicEvent)
+    assert event.title == "Strawberry Fields Forever"
+    assert event.artist == "The Beatles"
+    assert event.album == "Magic Mystery Tour"
