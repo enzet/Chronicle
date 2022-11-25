2d61cb9104fa50eb0ba0e09a0ff19ad1c47be345
Sergey Vartanov <me@enzet.ru>
2022-11-25 11:51:47 +0400

    Add test for listening audiobook

--- tests/test_command.py
+++ tests/test_command.py
@@ -3,6 +3,7 @@ from datetime import timedelta
 from chronicle.event.core import Event
 from chronicle.event.listen import (
     Interval,
+    ListenAudiobookEvent,
     ListenMusicEvent,
     ListenPodcastEvent,
 )
@@ -42,3 +43,21 @@ def test_listen_music() -> None:
     assert event.title == "Strawberry Fields Forever"
     assert event.artist == "The Beatles"
     assert event.album == "Magic Mystery Tour"
+
+
+def test_listen_audiobook() -> None:
+    """Test listening song command with title, artist, and album."""
+
+    timeline: Timeline = Timeline()
+    timeline.parse_command(
+        "2022-01-01T13:00:00/2022-01-01T14:00:00 audiobook idiot x1.25 00:00-10:00"
+    )
+
+    assert len(timeline) == 1
+    event: Event = timeline.events[0]
+    assert isinstance(event, ListenAudiobookEvent)
+    assert event.audiobook_id == "idiot"
+    assert event.speed == 1.25
+    assert event.interval == Interval(
+        from_=timedelta(), to_=timedelta(minutes=10)
+    )
