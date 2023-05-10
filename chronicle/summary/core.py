28500bdc11959dd721349d5940aa4a57efec95b6
Sergey Vartanov <me@enzet.ru>
2023-05-11 01:52:33 +0400

    Add sleeping time to summary

--- chronicle/summary/core.py
+++ chronicle/summary/core.py
@@ -5,6 +5,9 @@ from datetime import timedelta
 from chronicle.event.value import Language
 from chronicle.time import format_delta
 
+__author__ = "Sergey Vartanov"
+__email__ = "me@enzet.ru"
+
 
 @dataclass
 class Summary:
@@ -19,13 +22,19 @@ class Summary:
     )
     """Reading something in pages."""
 
+    sleep: float = 0
+    """Sleeping time in seconds."""
+
     def register_listen(self, duration: float, language: Language):
         """Register the number of seconds listened in language."""
         self.listen[language] += duration
 
     def register_read(self, pages: float, language: Language):
         """Register the number of pages read in language."""
-        self.listen[language] += pages
+        self.read[language] += pages
+
+    def register_sleep(self, duration: float):
+        self.sleep += duration
 
     def __str__(self):
         """Get human-readable text representation of the summary."""
@@ -37,5 +46,6 @@ class Summary:
                 f"    {key if key else '(Unknown)'}: "
                 f"{format_delta(timedelta(seconds=value))}\n"
             )
+        summary += f"Sleep: {format_delta(timedelta(seconds=self.sleep))}\n"
 
         return summary
