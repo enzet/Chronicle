c4f5053328962162bd52a0fa92ce0727243f3c34
Sergey Vartanov <me@enzet.ru>
2023-08-04 23:52:09 +0400

    Fix event tests

--- tests/test_event.py
+++ tests/test_event.py
@@ -8,24 +8,27 @@ from chronicle.event.art import ListenPodcastEvent
 __author__ = "Sergey Vartanov"
 __email__ = "me@enzet.ru"
 
+from event.value import Language
+
+from objects import Podcast
+
 
 def test_listen() -> None:
     """Test listen event parsing."""
 
     objects: Objects = Objects(
-        podcasts={
-            "inner_french": {
-                "title": "Intermediate French Podcast",
-                "language": "fr",
-            }
+        {
+            "inner_french": Podcast(
+                title="Intermediate French Podcast",
+                language=Language("fr"),
+            )
         }
     )
 
     data: dict[str, Any] = {
         "time": "2022-01-01",
         "podcast_id": "inner_french",
-        "kind": "podcast",
-        "episode": 15,
+        "episode": "15",
     }
     event: ListenPodcastEvent = ListenPodcastEvent(**data)
     assert event.podcast_id == "inner_french"
