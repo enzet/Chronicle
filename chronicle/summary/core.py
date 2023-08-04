4e5aa7e1f20edcd2f5ad318b349bfde00df8baef
Sergey Vartanov <me@enzet.ru>
2023-08-05 00:06:13 +0400

    Support summary for work, cook, and sport events

--- chronicle/summary/core.py
+++ chronicle/summary/core.py
@@ -1,4 +1,4 @@
-from collections import defaultdict
+from collections import defaultdict, Counter
 from dataclasses import dataclass, field
 from datetime import timedelta
 
@@ -24,6 +24,20 @@ class Summary:
     sleep: float = 0
     """Sleeping time in seconds."""
 
+    dishes: Counter = field(default_factory=lambda: Counter())
+
+    work: float = 0
+    """Working time in seconds."""
+
+    abs: int = 0
+    dips: int = 0
+    push_ups: int = 0
+    chin_ups: int = 0
+    squats: int = 0
+    burpees: int = 0
+    jumping_jacks: int = 0
+    russian_twists: int = 0
+
     def register_listen(self, duration: float, language: Language):
         """Register the number of seconds listened in language."""
         self.listen[language] += duration
@@ -35,9 +49,19 @@ class Summary:
     def register_sleep(self, duration: float):
         self.sleep += duration
 
+    def register_dish(self, dish: str):
+        self.dishes.update({dish: 1})
+
+    def register_work(self, duration: float):
+        self.work += duration
+
     def __str__(self):
         """Get human-readable text representation of the summary."""
         summary: str = ""
+
+        summary += "Read:\n"
+        for key, value in sorted(self.read.items(), key=lambda x: -x[1]):
+            summary += f"    {key if key else '(Unknown)'}: {value}\n"
 
         summary += "Listen:\n"
         for key, value in sorted(self.listen.items(), key=lambda x: -x[1]):
@@ -45,6 +69,13 @@ class Summary:
                 f"    {key if key else '(Unknown)'}: "
                 f"{format_delta(timedelta(seconds=value))}\n"
             )
+
         summary += f"Sleep: {format_delta(timedelta(seconds=self.sleep))}\n"
+
+        summary += f"Work: {format_delta(timedelta(seconds=self.work))}\n"
+
+        summary += "Dishes:\n"
+        for key, value in sorted(self.dishes.items(), key=lambda x: -x[1]):
+            summary += f"    {key}: {value}\n"
 
         return summary
